8b91662d7391de28e4424078109325abdf2af4d8
Felix <felix_b160423ec@nitc.ac.in>
2020-03-25 11:12:18 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,56 @@
+import os
+import random
+import pygame
+import math
+import sys
+
+# asdf
+
+class box(pygame.sprite.Sprite):
+    def __init__(self, color, width, height,x,y):
+       pygame.sprite.Sprite.__init__(self)
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+       self.rect = self.image.get_rect()
+       self.rect.center=(x,y)
+
+
+screen = pygame.display.set_mode((600, 700))
+speed=[2,-2]
+
+player=box((0,0,0),16, 16,54,64)
+item=[[box((0,0,0),16, 16, 16*i+i,16*j+j) for i in range(int(600/16)-1)]for j in range(10)]
+
+allSpritesLayered=pygame.sprite.LayeredUpdates()
+allSprites = pygame.sprite.Group()
+
+for i in item:
+    for j in i:
+        allSprites.add(j)
+allSprites.add(player)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    screen.fill((0, 0, 255))
+    allSpritesLayered.empty()
+    for sprite in allSprites:
+        allSpritesLayered.add(sprite,layer = sprite.rect.y)
+    allSpritesLayered.draw(screen)  
+    player.rect = player.rect.move(speed)
+    if player.rect.left < 0 or player.rect.right > 600:
+        speed[0] = -speed[0]
+    if player.rect.top < 0 or player.rect.bottom > 700:
+        speed[1] = -speed[1]
+    for i in allSprites:
+        if i==player:
+            continue
+        else:
+            if pygame.sprite.collide_rect(i,player):
+                if player.rect.top<i.rect.bottom or player.rect.bottom>i.rect.top:
+                    speed[1]=-speed[1]
+                if player.rect.left<i.rect.right or player.rect.right>i.rect.left:
+                    speed[0]=-speed[0]
+                allSprites.remove(i)
+    pygame.display.update()
